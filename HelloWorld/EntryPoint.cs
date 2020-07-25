d7490ff828eb26a622e417d10e1b5c347f0db589
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-07-25 19:43:02 +0100

    Initialise repo

---
+++ HelloWorld/EntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        Console.WriteLine("hello world");
+        Console.WriteLine("What's your name?");
+        string answer = Console.ReadLine();
+        Console.WriteLine(answer.ToUpper());
+    }
+}
+
